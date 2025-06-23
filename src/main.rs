a36a6ddb936571144b174dbe088ac512c956fee0
SvelteRust <oddharald@myhren.ai>
2025-06-23 16:03:34 +0200

    funny rust

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,12 @@ async fn main() -> Result<()> {
         clear_background(WHITE);
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_circle(screen_width() - 45.0, screen_height() - 45.0, 30.0, PURPLE);
 
         // Draw payload json
         let payload = Payload::Move { x: 10.0, y: 15.0 };
-        draw_text(
-            &serde_json::to_string(&payload)?,
-            100.0,
-            100.0,
-            40.0,
-            DARKGRAY,
-        );
+        let json = serde_json::to_string(&payload)?;
+        draw_text(&json, 150.0, 300.0, 40.0, BLACK);
         next_frame().await
     }
 }
